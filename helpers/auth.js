e7e2e7458c7b6242f528fb54a45e43b201a46b06
Segun <shegunolanitori@gmail.com>
2020-12-10 20:43:16 +0100

    feat: tokenand middleware helpers for auth

---
+++ helpers/auth.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken")
+
+const createToken = (user) => {
+    return jwt.sign({
+        user_id: user._id,
+    }, process.env.TOKEN_SECRET, {
+        expiresIn: "365d"
+    })
+}
+
+const verifyJwt = (req, res, next) => {
+    const authorization = req.headers["authorization"]
+    if (!authorization) {
+        return res.status(401).send("no authorization, you have to log in")
+    }
+    try {
+        const token = authorization.split(" ")[1]
+        const payload = jwt.verify(token, process.env.TOKEN_SECRET)
+        req.payload = payload
+        next()
+    } catch (err) {
+        res.status(401).send(err)
+
+    }
+}
+
+
+module.exports = {
+    createToken,
+    verifyJwt
+}
